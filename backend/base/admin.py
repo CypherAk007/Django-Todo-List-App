4ff26d08d872fcc0939ba271a323e566f35773d8
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-04-01 21:41:18 +0530

    Admin panel display

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ('task','status','updated_at')
+    search_fields = ('task',)
 
-admin.site.register(TodoList)
+admin.site.register(TodoList,TodoListAdmin)
